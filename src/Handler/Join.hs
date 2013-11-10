8961a65a3dd933835516159df3fe87e99e3ad6a0
Joel <me@joelt.io>
2013-11-09 22:16:23 -0800

    stuff

--- src/Handler/Join.hs
+++ src/Handler/Join.hs
@@ -1,11 +1,19 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ViewPatterns #-}
 
 module Handler.Join where
 
 import Control.Arrow
+import Control.Monad
+import qualified Data.ByteString.Lazy as B
+import Data.Char
 import Data.Text (pack)
+import Data.Text.Encoding
+import Data.Digest.Pure.SHA
+import qualified Data.Text as T
 import Data.String
 import Model.Types
+import System.IO
 import Text.Julius
 import Import
 
@@ -17,26 +25,33 @@ getJoinR = do
         $(widgetFile "join")
 
 postJoinR :: Handler Html
-postJoinR = error "postJoinR: not implemented"
+postJoinR = do
+    ((res, widget), enctype) <- runFormPost joinForm
+    case res of
+        FormSuccess person -> defaultLayout [whamlet|<p>#{show person}|]
+        _ -> defaultLayout $ do
+           setTitle "try again"
+           $(widgetFile "join")
 
 joinForm :: Form User
 joinForm ex = do
     (emailResult, emailView) <- mreq emailField (formControlAutofocus "Email") Nothing
     (passResult, passView) <- mreq passwordField (formControl "Password") Nothing
-    (passConfirmResult, passConfirmView) <- mreq passwordField (formControl "Password (again)") Nothing
-    (usernameResult, usernameView) <- mreq textField (formControl "Username (no spaces)") Nothing
+    (passConfirmResult, passConfirmView) <- mreq (checkBool ((>= 8) . T.length) ("Too short! (<8 characters)" :: Text) passwordField) (formControl "Password (again)") Nothing
+    (usernameResult, usernameView) <- mreq (checkBool (not . T.any isSpace) ("Spaces not allowed!" :: Text) textField) (formControl "Username (no spaces)") Nothing
     let selectOpts = "" { fsAttrs =
                             [ ("data-style", "btn-success")
                             , ("data-live-search", "true")
                             ] }
-    (puddingResult, puddingView) <- mreq (selectFieldList puddings) selectOpts Nothing
+    (puddingResult, puddingView) <- mreq (selectField defaultOptionsEnum) selectOpts Nothing
+    bytes <- liftIO $ withBinaryFile "/dev/urandom" ReadMode (fmap (decodeUtf8 . B.toStrict . bytestringDigest . sha1) . flip B.hGet 36)
     let user = User
             <$> emailResult
             <*> usernameResult
             <*> passResult
-            <*> pure "foo"
+            <*> pure bytes
             <*> puddingResult
-    return (user, $(widgetFile "join-form") >> toWidget $(juliusFile "templates/join-form.julius"))
+    return (user, $(widgetFile "join-form"))
     where
         puddings = map (pack . show &&& id) [minBound..maxBound :: PuddingType]
         formControlAutofocus (formControl -> f) =
@@ -45,3 +60,5 @@ joinForm ex = do
                    { fsLabel = fromString pl
                    , fsAttrs = [("class", "form-control"), ("placeholder", fromString pl)]
                    }
+        defaultOptionsEnum = fmap (prepend (Option "Favorite pudding" VanillaPudding "no pudding")) optionsEnum
+            where prepend m (OptionList a b) = OptionList (m:a) b
